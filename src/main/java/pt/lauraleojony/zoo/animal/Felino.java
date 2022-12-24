225f86adff92e6b8c2e0ae104917bb3b58321213
cacobaco <lcjmonizf09@gmail.com>
2022-12-24 20:29:11 +0000

    update felino and panthera

--- src/main/java/pt/lauraleojony/zoo/animal/Felino.java
+++ src/main/java/pt/lauraleojony/zoo/animal/Felino.java
@@ -1,9 +1,5 @@
 package pt.lauraleojony.zoo.animal;
 
-/**
- *
- * @author lcjmonizf09
- */
 public abstract class Felino extends Animal {
     
     public Felino(String nome, int idade, int atratividade, int apetiteReprodutivo) {
--- src/main/java/pt/lauraleojony/zoo/animal/Panthera.java
+++ src/main/java/pt/lauraleojony/zoo/animal/Panthera.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pt.lauraleojony.zoo.animal;
 
-/**
- *
- * @author lcjmonizf09
- */
 public abstract class Panthera extends Felino {
     
     public Panthera(String nome, int idade, int atratividade, int apetiteReprodutivo) {
-        super(nome, idade);
+        super(nome, idade, atratividade, apetiteReprodutivo);
     }
     
 }
